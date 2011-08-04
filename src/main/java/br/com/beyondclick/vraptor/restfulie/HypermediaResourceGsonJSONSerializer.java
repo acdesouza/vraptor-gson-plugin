f8c9d594d96e17efb99f1164b28096861d0f3339
Antonio Carlos da Graça Mota Durão de Souza <antonio.mota@corp.globo.com>
2011-08-04 17:08:26 -0300

    Fechar o JSON, serializado.

--- src/main/java/br/com/beyondclick/vraptor/restfulie/HypermediaResourceGsonJSONSerializer.java
+++ src/main/java/br/com/beyondclick/vraptor/restfulie/HypermediaResourceGsonJSONSerializer.java
@@ -41,11 +41,10 @@ public class HypermediaResourceGsonJSONSerializer extends GsonJSONSerializer {
 					list.add( new Link(t.getName(), config.getApplicationPath() + t.getUri()) );
 				}
 				linksConverted += getGson().toJson(list);
-				linksConverted += "}";
 			}
 		}
 
-		String hypermediaResourceConverted = defaultConversion.substring(0, defaultConversion.length() - 1) + linksConverted;
+		String hypermediaResourceConverted = defaultConversion.substring(0, defaultConversion.length() - 1) + linksConverted + "}";
 		return hypermediaResourceConverted;
 	}
 }
